ad9e68f99560d01100e313a486220a50d0ee38cd
Salah <105601398+salahalbasha@users.noreply.github.com>
2023-06-10 04:37:43 -0400

    Update files

--- improvements.py
+++
@@ -1,6 +0,0 @@
-# improvements for digital_cv
-
-"🏆 Hotel Booking Cancellation Prediction": {
-        "link": "https://pythonandvba.com/mytoolbelt/",
-        "skills_tools": "Exploratory Data Analysis, Classification, Logistic Regression, Support vector Machines, Decision Trees, Random Forest",
-    },
